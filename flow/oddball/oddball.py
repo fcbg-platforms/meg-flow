46dcf4977b6b37f5f23ec7b2d99947c2951ad107
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2024-11-29 14:17:34 +0100

    improve structure

--- flow/oddball/oddball.py
+++ flow/oddball/oddball.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
+import time
 from importlib.resources import files
 
 import zmq
+from psychopy.hardware.keyboard import Keyboard
 
 from ..utils._checks import check_type, check_value
 from ..utils._imports import import_optional_dependency
-from ..utils.logs import logger
+from ..utils.logs import logger, warn
 from ._config import (
     AUDIO_DEVICE,
     DURATION_ITI,
@@ -14,8 +16,8 @@ from ._config import (
     TRIGGER_ADDRESS,
     TRIGGERS,
 )
-from ._time import sleep
-from ._utils import _load_sounds, parse_trial_list
+from ._time import Clock, sleep
+from ._utils import _disable_psychopy_logs, _load_sounds, parse_trial_list
 
 _MESSAGES: dict[str, bool] = {"hold": True, "continue": False}
 _TRIAL_LIST_MAPPING: list[str] = [
@@ -55,8 +57,11 @@ def oddball(condition: str, mock: bool = False) -> None:
     fname = files("flow.oddball") / "trialList" / f"{condition}.txt"
     trials = parse_trial_list(fname)
     sounds = _load_sounds(trials, DURATION_STIM, AUDIO_DEVICE)
-    # prepare triggers
+    # prepare triggers and keyboard
     trigger = MockTrigger() if mock else ParallelPortTrigger(TRIGGER_ADDRESS)
+    keyboard = Keyboard()
+    with _disable_psychopy_logs():
+        keyboard.stop()
     # prepare fixation cross window
     input(">>> Press ENTER to start.")
     # main loop
@@ -73,19 +78,17 @@ def oddball(condition: str, mock: bool = False) -> None:
             sleep(DURATION_ITI - DURATION_STIM)
             continue
         logger.info("Trial %i / %i: %s", k, trials[-1][0], trial)
-        # retrieve trigger value and sound
-        if trial in TRIGGERS:
-            assert trial in ("standard", "target"), f"Error with trial ({k}, {trial})."
-            value = TRIGGERS[trial]
-        else:
-            assert trial.startswith("wav"), f"Error with trial ({k}, {trial})."
-            value = TRIGGERS["novel"]
-        # schedule sound, wait, and deliver triggers simultanouesly with the sound
+        # handle trigger and sound
         sounds[trial].play(when=ptb.GetSecs() + DURATION_STIM)
         sleep(DURATION_STIM)
-        trigger.signal(value)
+        trigger.signal(TRIGGERS[trial if trial in TRIGGERS else "novel"])
         counter += 1
-        sleep(DURATION_ITI - DURATION_STIM)
+        if trial in TRIGGERS:  # sanity-check the trial and handle inter-trial sleep
+            assert trial in ("standard", "target"), f"Error with trial ({k}, {trial})."
+            sleep(DURATION_ITI - DURATION_STIM)
+        else:
+            assert trial.startswith("wav"), f"Error with trial ({k}, {trial})."
+            _sleep_and_monitor_keyboard(DURATION_ITI - DURATION_STIM, keyboard)
     input(">>> Press ENTER to continue and close the window.")
 
 
@@ -100,3 +103,24 @@ def _check_message_for_hold(socket, poller) -> bool:
     else:
         hold = False
     return hold
+
+
+def _sleep_and_monitor_keyboard(duration: float, keyboard: Keyboard) -> None:
+    """Sleep and monitor the keyboard."""
+    clock = Clock()
+    keyboard.start()
+    duration *= 1e9
+    while True:
+        remaining_time = duration - clock.get_time_ns()  # nanoseconds
+        if remaining_time <= 0:
+            break
+        keys = keyboard.getKeys(keyList=["0"], waitRelease=True)
+        if len(keys) > 1:
+            warn("Multiple space key pressed simultaneously. Skipping.")
+            continue
+        elif len(keys) == 1:
+            logger.info("Response '0' received.")
+        if remaining_time >= 200000:  # 200 microseconds
+            time.sleep(remaining_time * 1e-9 / 2)
+    with _disable_psychopy_logs():
+        keyboard.stop()
